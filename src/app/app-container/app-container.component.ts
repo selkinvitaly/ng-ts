4796fd8e477cdf0d6ccb376a4850daa774e2d2aa
Igor Nepipenko <igornepipenko@gmail.com>
2016-08-29 11:47:48 +0300

    some comments (#1)

--- src/app/app-container/app-container.component.ts
+++ src/app/app-container/app-container.component.ts
@@ -1,6 +1,9 @@
 "use strict";
 
-import template from "./app-container.component.html";
+/**
+ * можно предложить такое решение , т.к. template это не модуль, но webpack коректно обработает это через loaders
+ * */
+let template = require("./app-container.component.html");
 
 export default {
   template: template
