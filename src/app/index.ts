435c8270bae7d1189f20f1fd762ded0a42876c38
Nepipenkoigor <igornepipenko@gmail.com>
2016-08-29 11:33:39 +0300

    some comments

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,10 @@
 "use strict";
 
-import angular from "angular";
-import router from "angular-ui-router";
+/**
+ * если посмотреть на исходник (types) то мы должны использовать as синтаксис
+ * */
+import * as angular from "angular";
+//import * as router from "angular-ui-router/commonjs/core";
 
 // config, run
 import { config as configFunction, run as runFunction } from "./router";
@@ -9,8 +12,12 @@ import { config as configFunction, run as runFunction } from "./router";
 // components
 import appContainerComponent from "./app-container/app-container.component";
 
-const app = angular.module("app", [router]);
+/**
+ *
+ * насколько я помню тут надо обьявить так ui-router (по названию) , так как сам ui-router будут в vendor bundle
+ */
 
+const app = angular.module("app",['ui-router']);
 app.component("appContainer", appContainerComponent);
 
 app.config(configFunction);
